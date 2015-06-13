a5304bcd58218de23badda064e0abf4a5abaa4a9
ethan <e.ozelius@gmail.com>
2015-06-12 20:45:20 -0400

    added dynamic page titles

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,6 +4,6 @@ class WelcomeControllerTest < ActionController::TestCase
 	test "home page should respond & have proper title" do
 		get 'index'
 		assert_response :success
-		assert_select "title", "Home"
+		assert_select "title", "Home | myApp"
 	end
 end
