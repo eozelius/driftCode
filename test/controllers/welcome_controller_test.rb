533f9102045d2358d196eabc8a03cd792c17ad8e
ethan <e.ozelius@gmail.com>
2015-06-12 20:20:23 -0400

    delete static-pages controller; added home page

---
+++ test/controllers/welcome_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class WelcomeControllerTest < ActionController::TestCase
+	test "home page should respond & have proper title" do
+		get 'index'
+		assert_response :success
+		assert_select "title", "Home"
+	end
+end
