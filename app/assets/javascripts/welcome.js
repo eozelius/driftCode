8f32c52b7c6aadc823f2150f6a95df7b5630105b
ethan <e.ozelius@gmail.com>
2015-06-21 19:14:50 -0400

    Leaflet with my leafletJS account

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,10 +1,7 @@
 $(document).ready(function(){
-	console.log("dom loaded");
-
 	// Hover Nav li -> display name
-	$('#main ul li').hover( function(){
+	$('#main ul li').hover(function(){
 		var showP = $(this).find('p').first().toggle('fast');
 	});
-
 });
 
