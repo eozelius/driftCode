836ed1e2f7b1e4cc8fc9c1611bd0cdde0c406bba
ethan <e.ozelius@gmail.com>
2015-06-25 19:24:10 -0400

    removed old test; allow for post editing and deleting.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,17 @@ class PostsController < ApplicationController
 	end
 
 	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title, :body))
+			redirect_to @post
+		else
+			render 'edit'
+		end
 	end
 
 	def show
@@ -26,8 +37,11 @@ class PostsController < ApplicationController
 
 
 	def destroy
-	end
+		@post = Post.find(params[:id])
+		@post.delete
 
+		redirect_to root_path
+	end
 
 	private
 		def post_params
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -5,9 +5,6 @@ class WelcomeControllerTest < ActionController::TestCase
 		get 'index'
 			assert_response :success
 			assert_select "title", "home | driftingCoder"
-		get 'blog'
-			assert_response :success
-			assert_select "title", "blog | driftingCoder"
 		get 'code'
 			assert_response :success
 			assert_select "title", "code | driftingCoder"
