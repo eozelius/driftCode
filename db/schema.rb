acf3267f97a53300e25ef30e133ad4d0c01e0f12
ethan <e.ozelius@gmail.com>
2015-07-12 17:04:35 -0400

    Ch 9 finished

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709004928) do
+ActiveRecord::Schema.define(version: 20150712204308) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150709004928) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "Simon Heizen",
+             email: "SHeizen@gubenheimen.org",
+             password:              "japan123",
+             password_confirmation: "japan123")
+
+99.times do |n|
+  name  = "Simon Heizen"
+  email = "SHeizen-#{n+1}@gubenheimen.org"
+  password = "japan123"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
