99c795090acde307827f813ec65130c1a16310ea
ethan ozelius <e.ozelius@gmail.com>
2015-11-22 18:04:12 -0500

    dashboard created and styled

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,21 +1,4 @@
 module PostsHelper
-	def has_any_posts user
-		user_posts = []
-
-		if user.posts.length <= 1
-			return false
-		else
-			user.posts.each do |post|
-				# if any post attr is nil, do not return
-				# Post the post only if all attr are defined except picture
-				if !post.body.nil? || !post.title.nil? || !post.user_id.nil? || !post.id.nil? || !post.created_at.nil? || !post.updated_at.nil?
-					user_posts.push(post)
-				end
-			end
-		end
-		return user_posts
-	end
-
 	def user_is_home user
 		# byebug
 		if !logged_in? || current_user.nil? || user.nil?
