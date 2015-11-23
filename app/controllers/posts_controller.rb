8af71484004c3c76d381ccd2ad0a3767ce481cc4
ethan ozelius <e.ozelius@gmail.com>
2015-11-23 18:09:36 -0500

    fix bug that doesn't allow users to delete profile

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-	before_action :logged_in_user,	only: [:create, :destroy]
-	before_action :correct_user, 		only: :destroy
+	before_action :logged_in_user,	only: [:create,  :destroy]
+	before_action :correct_user, 		only: [:destroy, :update]
 
 	# ror_tut version
 	def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_action :correct_user,   only: [:edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -47,8 +46,8 @@ class UsersController < ApplicationController
 
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
-    redirect_to users_url
+    flash[:success] = "User deleted successfully"
+    redirect_to root_url
   end
 
   private
@@ -59,7 +58,12 @@ class UsersController < ApplicationController
     # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
+      if current_user?(@user) || current_user.admin?
+        return true
+      else 
+        flash[:danger] = "You do not have permission to do that. wwjd?"
+        redirect_to(root_path)
+      end
     end
 
     # Confirms an admin user.
