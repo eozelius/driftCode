10b05a643d8abf7addbea9db54c5410fb62157ee
ethan ozelius <e.ozelius@gmail.com>
2016-02-10 22:05:04 -0500

    First 3 steps of create driftmap done;

--- app/assets/javascripts/driftmap.js
+++ app/assets/javascripts/driftmap.js
@@ -8,24 +8,47 @@ $(document).ready(function(){
 	});
 });
 
-var driftMapForm = {
-	name: 'my drift map',
-	init: [40.735, -73.890],
-	zoom: 18,
-}
+var driftMapForm = {}
 
-
+/*
+	todo: turn all the hiding/showing into a function;
+				add a back button 
+*/
 var next_step = function(step) {
-	$('.current-step p').remove();
-	var instructionsHtml = ''
-
 	switch(step) {
     case 1:
-    	$('p.instructions').text('Initialize your driftMap.');
-    	instructionsHtml = '<p>Position the Map with coordinates, and zoom that you would like.</p>'
-    	$('.current-step').append(instructionsHtml);
+    	// Set form properties
+    	var title = $('#driftmap-title').val();
+    	console.log("title: " + title);
+    	driftMapForm.map_title = title;
+    	// remove instructions
+    	$('.create-step-1').remove();
+    	// add next instructions
+    	$('.create-step-2').slideDown();
     	break;
+    
     case 2:
+    	var description = $('#driftmap-body').val();
+    	console.log("description: " + description)
+    	driftMapForm.map_description = description;
+    	$('.create-step-2').remove();
+    	$('.create-step-3').slideDown();
+    	break;
+
+    case 3:
+    	var zoom = map.getZoom();
+    	var initCenter = map.getCenter();
+    	console.log("init: " + JSON.stringify(initCenter));
+    	console.log("zoom: " + zoom)
+    	driftMapForm.initPt = initCenter;
+    	driftMapForm.initZoom = zoom;
+    	$('.create-step-3').remove();
+    	$('.create-step-4').slideDown();
+    	break;
+
+    case 4:
+    	
+
 
 
 
