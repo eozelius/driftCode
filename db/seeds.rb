5839555d80d386b49360c150bbe0adf1a2ca776e
ethan ozelius <e.ozelius@gmail.com>
2016-09-14 21:56:02 -0400

    mobilize profilepage and switch the seeds a little

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,15 @@ ethan = User.create!(
   activated: true,
   activated_at: Time.zone.now,
   admin: true,
-  from: 'concord nh',
-  gps: 'Sunnyside ny',
+  from: 'concord new hampshire',
+  gps: 'queens new york',
   profile_pic: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/ethan.jpg")
 )
 
 # Create driftmap for ethan
 ethan.post = Post.create(
   title:  "./driftmap",
-  body:   "just a computer hacker trying to get some fresh air",
+  body:   "just a computer hacker trying to get some fresh air.",
   init_x: 26.549222577692,
   init_y: -83.0126953125,
   init_zoom: 4
@@ -28,9 +28,9 @@ queens = ethan.post.blips.create!(
   y: -73.922
 )
 
-queens.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/queens/queens.jpg"))
 queens.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/queens/queens_graffiti.JPG"))
 queens.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/queens/queens_mets.jpg"))
+queens.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/queens/queens.jpg"))
 queens.save!
 
 concord = ethan.post.blips.create!(
@@ -40,8 +40,8 @@ concord = ethan.post.blips.create!(
   body: 'A subtle, yet fantastic town in the heart of NH.  NH is flat out best at being NH.  And it doens\'t need to be anything else.  NH is a blethera of kind people, scenic nature, and classic New England Culture, and my personal haven when the concrete jungle becomes oppressive.'.html_safe
 )
 
-concord.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/concord/concord.jpg"))
 concord.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/concord/concord_castros.jpg"))
+concord.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/concord/concord.jpg"))
 concord.save!
 
 medellin = ethan.post.blips.create!(
@@ -51,10 +51,10 @@ medellin = ethan.post.blips.create!(
   y: -75.59,
 )
 
-medellin.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/medellin/medellin.jpg"));
 medellin.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/medellin/medellin_itigui.JPG"));
 medellin.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/medellin/medellin_night.JPG"));
 medellin.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/medellin/medellin_plane.JPG"));
+medellin.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/medellin/medellin.jpg"));
 medellin.save!
 
 bogota = ethan.post.blips.create!(
@@ -64,10 +64,10 @@ bogota = ethan.post.blips.create!(
   y: -74.082
 )
 
+bogota.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/bogota/bogota_monsarate.jpg"))
 bogota.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/bogota/bogota_plaza_bolivar.jpg"))
 bogota.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/bogota/bogota_candelaria.jpg"))
 bogota.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/bogota/bogota_fut.jpg"))
-bogota.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/bogota/bogota_monsarate.jpg"))
 bogota.save!
 
 if Rails.env.development? || Rails.env.test?
