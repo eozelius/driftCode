1133e5fcfebea19848be6577806f7098bb2767d0
ethan ozelius <e.ozelius@gmail.com>
2016-09-15 23:09:31 -0400

    unstable

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,11 +1,9 @@
 $(function(){
 	$('.toggle-sidenav').on('click', function(){
 		var active = $('#main').hasClass('active');;
-
 		$('#main').stop().animate({
 			left: active ? '-65px' : '0px'
 		});
-
 		$('#main').toggleClass('active').css('display', 'block');
 	});
 });
