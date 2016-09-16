4fa9828e633b5ac3f82a84dcabe9b557648de793
ethan ozelius <e.ozelius@gmail.com>
2016-09-15 20:57:10 -0400

    add side nav

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('.toggle-sidenav').on('click', function(){
+		var active = $('#main').hasClass('active');;
+
+		$('#main').stop().animate({
+			left: active ? '-65px' : '0px'
+		});
+
+		$('#main').toggleClass('active').css('display', 'block');
+	});
+});
