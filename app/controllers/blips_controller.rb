c4e377b699afbd0b46a5c4fb894407c969ea0bdc
ethan ozelius <e.ozelius@gmail.com>
2016-09-18 20:54:19 -0400

    asdf

--- app/controllers/blips_controller.rb
+++ app/controllers/blips_controller.rb
@@ -1,6 +1,5 @@
 class BlipsController < ApplicationController
 	before_action :logged_in_user
-	before_action :correct_user
 
 	def destroy
 		blip = Blip.find(params[:id])
@@ -11,17 +10,4 @@ class BlipsController < ApplicationController
 		flash[:success] = "Blip deleted"
 		redirect_to user
 	end
-
-	private
-		# Confirms the correct user.
-		def correct_user
-		  user = User.find(params[:id])
-
-		  if current_user?(user) || current_user.admin?
-		    return true
-		  else
-		    flash[:danger] = "You do not have permission to do that. wwjd?"
-		    redirect_to(root_path)
-		  end
-		end
 end
--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,24 +1,10 @@
 class RoutesController < ApplicationController
 	before_action :logged_in_user
-	before_action :correct_user
 
 	def destroy
 		route = Route.find(params[:id])
 		route.destroy
 
-		redirect_to current_user
+		redirect_to root_path
 	end
-
-	private
-		# Confirms the correct user.
-		def correct_user
-		  user = User.find(params[:id])
-
-		  if current_user?(user) || current_user.admin?
-		    return true
-		  else
-		    flash[:danger] = "You do not have permission to do that. wwjd?"
-		    redirect_to(root_path)
-		  end
-		end
 end
