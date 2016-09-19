6689c58b8fb20eefff038a375a0729954eae46b8
ethan ozelius <e.ozelius@gmail.com>
2016-09-19 11:12:14 -0400

    whitespace change, nothing important

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,5 +1,5 @@
 class Route < ActiveRecord::Base
   belongs_to :post
   has_many :route_points, dependent: :destroy
-	has_many :blips, dependent: :destroy
+  has_many :blips, dependent: :destroy
 end
