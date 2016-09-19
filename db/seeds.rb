85d941c8cabc892f7e251eddf0454ced3389ac89
ethan ozelius <e.ozelius@gmail.com>
2016-09-19 12:40:21 -0400

    Seed.rb Routes now have blips

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,61 +15,11 @@ ethan = User.create!(
 # Create driftmap for ethan
 ethan.post = Post.create(
   title:  "./driftmap",
-  body:   "just a computer hacker trying to get some fresh air.",
+  body:   "my adventures and travels as a drifting computer hacker.",
   init_x: 0.924206230200376,
   init_y: -34.2400771379471,
   init_zoom: 2
 )
-
-# StandAlone Blips
-queens = ethan.post.blips.create!(
-  title: 'Sunnyside Queens',
-  body: 'Queens NY.  From Flushing to Astoria to Jamaica, for centuries the world has landed in Queens to embark on a new life in the United States.  The culture, diversity and unforgettable food has made Queens one of the greatest places in the world to live, and a must-see for US and World citizens alike.'.html_safe,
-  x: 40.743,
-  y: -73.922
-)
-
-queens.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/queens/queens_graffiti.JPG"))
-queens.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/queens/queens_mets.jpg"))
-queens.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/queens/queens.jpg"))
-queens.save!
-
-concord = ethan.post.blips.create!(
-  x: 43.206,
-  y: -71.547,
-  title: 'Concord NH',
-  body: 'A subtle, yet fantastic town in the heart of NH.  NH is flat out best at being NH.  And it doens\'t need to be anything else.  NH is a blethera of kind people, scenic nature, and classic New England Culture, and my personal haven when the concrete jungle becomes oppressive.'.html_safe
-)
-
-concord.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/concord/concord_castros.jpg"))
-concord.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/concord/concord.jpg"))
-concord.save!
-
-medellin = ethan.post.blips.create!(
-  title: 'Medellin Colombia',
-  body: 'Without a question, Paisas, (the inhabitants of Medellin) are the friendliest people I\'ve come across in all my travels.  In Christmas this wonderland takes on a purple tint, and the entire city lights up as every home dons lights.  Built in the bottom of a valley, every view is spectacular, the city runs up from the river into the walls of the valley that is not to be missed.'.html_safe,
-  x: 6.242,
-  y: -75.59,
-)
-
-medellin.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/medellin/medellin_itigui.JPG"));
-medellin.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/medellin/medellin_night.JPG"));
-medellin.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/medellin/medellin_plane.JPG"));
-medellin.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/medellin/medellin.jpg"));
-medellin.save!
-
-bogota = ethan.post.blips.create!(
-  title: 'Bogota Colombia',
-  body: 'The Capital and heart of business in Colombia, Bogoat boasts a massive sprawling population of over 9 million inhabitants.  Bogota is a great window into the lives of everyday Colombians, their joy, dance, activism and struggle.'.html_safe,
-  x: 4.642,
-  y: -74.082
-)
-
-bogota.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/bogota/bogota_monsarate.jpg"))
-bogota.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/bogota/bogota_plaza_bolivar.jpg"))
-bogota.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/bogota/bogota_candelaria.jpg"))
-bogota.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/bogota/bogota_fut.jpg"))
-bogota.save!
 
 # Routes
 colombia = ethan.post.routes.create!(
@@ -102,6 +52,58 @@ europe.route_points.build( x: 46.8667638, y: 8.642395, order: 6 )
 europe.route_points.build( x: 46.4548893, y: 11.3274536, order: 7 )
 europe.route_points.build( x: 47.4503798, y: 18.9975586, order: 8 )
 europe.save!
+
+# Blips
+medellin = colombia.blips.create!(
+  title: 'Medellin Colombia',
+  body: 'Without a question, Paisas, (the inhabitants of Medellin) are the friendliest people I\'ve come across in all my travels.  In Christmas this wonderland takes on a purple tint, and the entire city lights up as every home dons lights.  Built in the bottom of a valley, every view is spectacular, the city runs up from the river into the walls of the valley that is not to be missed.'.html_safe,
+  x: 6.242,
+  y: -75.59,
+  post_id: ethan.post.id
+)
+
+medellin.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/medellin/medellin_itigui.JPG"));
+medellin.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/medellin/medellin_night.JPG"));
+medellin.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/medellin/medellin_plane.JPG"));
+medellin.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/medellin/medellin.jpg"));
+medellin.save!
+
+bogota = colombia.blips.create!(
+  title: 'Bogota Colombia',
+  body: 'The Capital and heart of business in Colombia, Bogoat boasts a massive sprawling population of over 9 million inhabitants.  Bogota is a great window into the lives of everyday Colombians, their joy, dance, activism and struggle.'.html_safe,
+  x: 4.642,
+  y: -74.082,
+  post_id: ethan.post.id
+)
+
+bogota.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/bogota/bogota_monsarate.jpg"))
+bogota.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/bogota/bogota_plaza_bolivar.jpg"))
+bogota.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/bogota/bogota_candelaria.jpg"))
+bogota.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/bogota/bogota_fut.jpg"))
+bogota.save!
+
+queens = ethan.post.blips.create!(
+  title: 'Sunnyside Queens',
+  body: 'Queens NY.  From Flushing to Astoria to Jamaica, for centuries the world has landed in Queens to embark on a new life in the United States.  The culture, diversity and unforgettable food has made Queens one of the greatest places in the world to live, and a must-see for US and World citizens alike.'.html_safe,
+  x: 40.743,
+  y: -73.922
+)
+
+queens.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/queens/queens_graffiti.JPG"))
+queens.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/queens/queens_mets.jpg"))
+queens.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/queens/queens.jpg"))
+queens.save!
+
+concord = ethan.post.blips.create!(
+  x: 43.206,
+  y: -71.547,
+  title: 'Concord NH',
+  body: 'A subtle, yet fantastic town in the heart of NH.  NH is flat out best at being NH.  And it doens\'t need to be anything else.  NH is a blethera of kind people, scenic nature, and classic New England Culture, and my personal haven when the concrete jungle becomes oppressive.'.html_safe
+)
+
+concord.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/concord/concord_castros.jpg"))
+concord.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/concord/concord.jpg"))
+concord.save!
 
 if Rails.env.development? || Rails.env.test?
   # Create Dummy Users
