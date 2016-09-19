8e64e0ba4a05cdd7191301ef7c29df2389627cec
ethan ozelius <e.ozelius@gmail.com>
2016-09-19 13:15:57 -0400

    add content to my colombia route

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,14 +27,14 @@ colombia = ethan.post.routes.create!(
   description: 'A gonzo style adventure into the heart and fringes of this South American Gem.'
 )
 
-colombia.route_points.build( x: 4.6077624, y: -74.0745113, order: 0 )
-colombia.route_points.build( x: 6.2807872, y: -75.5511055, order: 1 )
-colombia.route_points.build( x: 5.1073584, y: -75.5126810, order: 2 )
-colombia.route_points.build( x: 4.8564869, y: -75.6857157, order: 3 )
-colombia.route_points.build( x: 4.6671150, y: -75.5951801, order: 4 )
-colombia.route_points.build( x: 3.5644319, y: -76.5052577, order: 5 )
-colombia.route_points.build( x: 3.7782255, y: -77.1864100, order: 6 )
-colombia.route_points.build( x: 4.4904739, y: -74.1196424, order: 7 )
+colombia.route_points.build( x: 4.6077624, y: -74.0745113, order: 0 ) # bogota
+colombia.route_points.build( x: 6.2807872, y: -75.5511055, order: 1 ) # medellin
+colombia.route_points.build( x: 5.1073584, y: -75.5126810, order: 2 ) # manizales
+colombia.route_points.build( x: 4.8564869, y: -75.6857157, order: 3 ) # pereira
+colombia.route_points.build( x: 4.6671150, y: -75.5951801, order: 4 ) # Salento
+colombia.route_points.build( x: 3.5644319, y: -76.5052577, order: 5 ) # Cali
+colombia.route_points.build( x: 3.8408223, y: -76.8986596, order: 6 ) # San Cipriano
+colombia.route_points.build( x: 4.6501713, y: -74.0550950, order: 7 ) # Bogota x2
 colombia.save!
 
 europe = ethan.post.routes.create!(
@@ -43,7 +43,7 @@ europe = ethan.post.routes.create!(
 )
 
 europe.route_points.build( x: 55.6895847, y: 12.5706253, order: 0 )
-europe.route_points.build( x: 53.552139, y: 9.9933014, order: 1 )
+europe.route_points.build( x: 53.552139,  y: 9.9933014, order: 1 )
 europe.route_points.build( x: 52.5216526, y: 13.3855019, order: 2 )
 europe.route_points.build( x: 52.3786382, y: 4.9043999, order: 3 )
 europe.route_points.build( x: 48.8582776, y: 2.3507652, order: 4 )
@@ -57,8 +57,8 @@ europe.save!
 medellin = colombia.blips.create!(
   title: 'Medellin Colombia',
   body: 'Without a question, Paisas, (the inhabitants of Medellin) are the friendliest people I\'ve come across in all my travels.  In Christmas this wonderland takes on a purple tint, and the entire city lights up as every home dons lights.  Built in the bottom of a valley, every view is spectacular, the city runs up from the river into the walls of the valley that is not to be missed.'.html_safe,
-  x: 6.242,
-  y: -75.59,
+  x: 6.2807872, 
+  y: -75.5511055,
   post_id: ethan.post.id
 )
 
@@ -71,8 +71,8 @@ medellin.save!
 bogota = colombia.blips.create!(
   title: 'Bogota Colombia',
   body: 'The Capital and heart of business in Colombia, Bogoat boasts a massive sprawling population of over 9 million inhabitants.  Bogota is a great window into the lives of everyday Colombians, their joy, dance, activism and struggle.'.html_safe,
-  x: 4.642,
-  y: -74.082,
+  x: 4.6077624, 
+  y: -74.0745113,
   post_id: ethan.post.id
 )
 
@@ -81,6 +81,54 @@ bogota.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_
 bogota.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/bogota/bogota_candelaria.jpg"))
 bogota.blip_images.build(image: File.open("#{Rails.root}/app/assets/images/blip_images/ethan/bogota/bogota_fut.jpg"))
 bogota.save!
+
+manizales = colombia.blips.create!(
+  title: 'Manizales Colombia',
+  body: 'I packed my bag in a hurry to head south to Manizales for the city Feria.  The night I arrived I instantly made friends with a dude from manizales at a salsa concert.',
+  x: 5.1073584, 
+  y: -75.5126810,
+  post_id: ethan.post.id
+)
+
+pereira = colombia.blips.create!(
+  title: 'Pereira Colombia',
+  body: 'In Pereira, the lower points of the digital nomal lifestyle took a bit of a toll.  Work was excessive and friends were few and far between.  As in, I was literally the only guest at my residency.',
+  x: 4.8564869, 
+  y: -75.6857157,
+  post_id: ethan.post.id
+)
+
+salento = colombia.blips.create!(
+  title: 'Salento Colombia',
+  body: 'Finally, what Colombia is famous for, the delicious coffee.  If pereira was the place where the digital nomad lifestyle left me feeling like I got hit by a car, in Salento my lack of planning left me feeling like I fell into a Volcano.  Colombias coffee zone is one of the most beautiful places I have ever visited, and staying only an afternoon is an absolute travesty.  In spite of my procrastination, I rented a bicycle and ventured into the hills above the town and it was one of the greatest rides I have ever undertaken.'.html_safe,
+  x: 4.6671150, 
+  y: -75.5951801,
+  post_id: ethan.post.id
+)
+
+cali = colombia.blips.create!(
+  title: 'Cali Colombia',
+  body: 'El tierra de baile salsa.  Cali lives and breathes salse and nothing but.  I quickly relized my preconceptions that learning salsa in two classes was 100% incorrect, so I cut my loses and made some of the greatest friends in the world instead.'.html_safe,
+  x: 3.5644319, 
+  y: -76.5052577,
+  post_id: ethan.post.id
+)
+
+cipriano = colombia.blips.create!(
+  title: 'San Cipriano Colombia',
+  body: 'by far one of the greatest assets is the diversity in region, driving for only a couple hours can bring you from the sweltering heat of the coasts, to the freezing mountains of Bogota.  San Cipriano is not a far drive from Cali Colombia, but the change in scenery is staggering.  San Cipriano is the (only), most dense and genuine jungle I have ever traveled in.'.html_safe,
+  x: 3.8408223, 
+  y: -76.8986596,
+  post_id: ethan.post.id
+)
+
+bogota_2 = colombia.blips.create!(
+  title: 'Returning to Bogota Colombia',
+  body: 'I have visited bogota on many occasions, but this time would be like no other.',
+  x: 4.6501713, 
+  y: -74.0550950,
+  post_id: ethan.post.id
+)
 
 queens = ethan.post.blips.create!(
   title: 'Sunnyside Queens',
