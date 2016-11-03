e73592496dcfa2011bcc3d36b4a61aaca9005262
ethan ozelius <e.ozelius@gmail.com>
2016-11-03 13:06:34 -0400

    initial Routes New view setup

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,54 +1,12 @@
 class RoutesController < ApplicationController
 	before_action :logged_in_user
 
-	def create
-		post = current_user.post
-		
-		route = post.routes.new(
-			title: params[:title],
-			description: params[:description]
-		)
-
-		if route.save && post.save
-			status = 200
-			message = 'success'
-		else
-			status = 500
-			message = 'failure'
-		end
-
-		render json: { 
-			status:  status,
-			message: message,
-			title:   route.reload.title,
-			id: 		 route.id
-		}
+	def new
+		@user = current_user
+		@route = @user.post.routes.new
 	end
 
-	def update
-		# todo implement strong params and identity checking
 
-		route = Route.find(params[:id])
-
-		route.update_attributes(
-			title: params[:title],
-			description: params[:body]
-		)
-
-		if route.save
-			status = 200
-			message = 'success'
-		else
-			status = 500
-			message = 'failure'
-		end
-
-		render json: { 
-			status:  status,
-			message: message,
-			title:   route.reload.title
-		}
-	end
 
 	def destroy
 		route = Route.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  resources :routes,       only: [:create, :destroy]
+  resources :routes
   resources :route_points, only: [:create, :update, :destroy]
   resources :blips
   resources :blip_images,  only: [:create, :destroy]
