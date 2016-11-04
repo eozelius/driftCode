b95587a01d54eded88eb87e896df73abbe5d52a7
ethan ozelius <e.ozelius@gmail.com>
2016-11-04 14:03:22 -0400

    very initial commit of timeline working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @post = @user.post
-    @post.blips.any? ? @blips = @post.blips.order(created_at: :desc) : @blips = nil
+    @post.blips.any?  ? @blips  = @post.blips.order(created_at: :desc) : @blips  = nil
+    #@post.routes.any? ? @routes = @post.routes.order(created_at: :desc): @routes = nil
+    @post.routes.any? ? @route = @post.routes.second : @route = nil
   end
 
   def update
