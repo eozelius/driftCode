efe1287704e75f87aa77a0628f1b26a7c11854d2
ethan ozelius <e.ozelius@gmail.com>
2016-11-04 09:25:17 -0400

    create new route working well yaaaa

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -6,7 +6,40 @@ class RoutesController < ApplicationController
 		@route = @user.post.routes.new
 	end
 
+	def create
+		@user = current_user
+		@post = current_user.post
+		@route = Route.new(route_params)
+		@route.post_id = @post.id
 
+		if @route.save
+			# user choose to create a new blip
+			if params[:new_blip].present?
+				@blip = Blip.new(
+					title: params[:blip_title],
+					body:  params[:blip_description],
+					x: 		 params[:blip_x],
+					y:  	 params[:blip_y]
+				)
+				@blip.post_id = @post.id
+				@blip.route_id = @route.id
+
+				if params[:photo].present?
+					params[:photo].each do |image|
+						@blip.blip_images.build(image: image[1])
+					end
+				end
+				@blip.save
+			else
+				flash[:danger] = 'whoops, you have to create a new blip, or use an existing one'
+				render 'new'
+			end
+			flash[:success] = 'journey created successfully'
+			redirect_to current_user
+		else
+			render 'new'
+		end
+	end
 
 	def destroy
 		route = Route.find(params[:id])
@@ -14,4 +47,9 @@ class RoutesController < ApplicationController
 
 		redirect_to current_user
 	end
+
+	private
+		def route_params
+			params.require(:route).permit(:title, :description)
+		end
 end
