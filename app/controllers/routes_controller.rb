24b527e4295f87e1311ba66482a3707c406e4020
ethan ozelius <e.ozelius@gmail.com>
2016-11-22 15:43:55 -0500

    asdf

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -15,7 +15,6 @@ class RoutesController < ApplicationController
 		if @route.save
 			# user choose to create a new blip
 			if params[:new_blip].present?
-
  				date = Date.new(params[:blip_date].slice(0, 4).to_i, 
 												params[:blip_date].slice(4, 2).to_i, 
 												params[:blip_date].slice(6, 2).to_i);
@@ -40,6 +39,7 @@ class RoutesController < ApplicationController
 				flash[:danger] = 'whoops, you have to create a new blip, or use an existing one'
 				render 'new'
 			end
+
 			flash[:success] = 'journey created successfully'
 			redirect_to current_user
 		else
@@ -52,6 +52,44 @@ class RoutesController < ApplicationController
 		@post = current_user.post
 		@route = Route.find(params[:id])
 		@blips = @route.blips
+	end
+
+	def update
+		@route = Route.find(params[:id])
+		@user  = current_user
+		@post  = @user.post
+
+		if @route.update_attributes(route_params)
+			# user choose to create a new blip
+			if params[:new_blip].present?
+				date = Date.new(params[:blip_date].slice(0, 4).to_i, 
+												params[:blip_date].slice(4, 2).to_i, 
+												params[:blip_date].slice(6, 2).to_i);
+
+				@blip = Blip.new(
+					title: params[:blip_title],
+					body:  params[:blip_description],
+					x: 		 params[:blip_x],
+					y:  	 params[:blip_y],
+					date:  date
+				)
+				@blip.post_id = @post.id
+				@blip.route_id = @route.id
+
+				# Any Photos to new blip?
+				if params[:photo].present?
+					params[:photo].each do |image|
+						@blip.blip_images.build(image: image[1])
+					end
+				end
+				@blip.save
+			end
+
+			flash[:success] = 'Journey Updated'
+			redirect_to current_user
+		else
+			render 'edit'
+		end
 	end
 
 	def show
