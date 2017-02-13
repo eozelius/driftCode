589ddd95126cbad59ccc6e00933a76ab4efd17e9
ethan ozelius <e.ozelius@gmail.com>
2017-02-13 11:04:10 -0500

    remove route_point model from app

--- app/models/route_point.rb
+++
@@ -1,8 +0,0 @@
-class RoutePoint < ActiveRecord::Base
-  belongs_to :route
-
-  validates :x, presence: true
-  validates :y, presence: true
-  validates :order, presence: true
-  validates :route_id, presence: true
-end
