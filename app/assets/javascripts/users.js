6c6f76225e3ef63b9a7e9e3edcf1347dd43928e5
ethan ozelius <e.ozelius@gmail.com>
2017-02-23 15:37:40 -0500

    Controller bases JS files will test the action before executing action specific code

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,22 +1,22 @@
 /* NEW ACTION */
 $(function(){
-  // Cannot upload images larger than 3M
-  $('.signup').on('click', function(){
-    $('#new_user input[type="text"]').each(function(){
-      $(this).val(xss_trim($(this).val()));
+  if($('#users_new').length){
+    // Cannot upload images larger than 3M
+    $('.signup').on('click', function(){
+      $('#new_user input[type="text"]').each(function(){
+        $(this).val(xss_trim($(this).val()));
+      });
+      $('#new_user').submit();
     });
 
-    $('#new_user').submit();
-  });
-
-  $('#user_profile_pic').on('change', function() {
-    var size_in_megabytes = this.files[0].size/1024/1024;
-    if (size_in_megabytes > 3){
-      alert('Maximum file size is 3MB. Please choose a smaller file.');
-      return false;
-    }
-  });
-})
+    $('#user_profile_pic').on('change', function() {
+      var size_in_megabytes = this.files[0].size/1024/1024;
+      if (size_in_megabytes > 3){
+        alert('Maximum file size is 3MB. Please choose a smaller file.');
+        return false;
+      }
+    });
+  }
 
 /* CREATE ACTION */
 
@@ -24,43 +24,45 @@ $(function(){
 
 
 /* SHOW ACTION */
-$(function(){
-  $.ajax({
-    url: '/api_profile_page',
-    method: 'POST',
-    dataType: 'JSON',
-    data: { 
-      id: $('#user_id').val()
-    },
-    complete: function(response){
-      var r = response.responseJSON
-      var home = $('#user_is_home').length ? true : false
-      window.r = r
+  if($('#users_show').length){
+    $.ajax({
+      url: '/api_profile_page',
+      method: 'POST',
+      dataType: 'JSON',
+      data: { 
+        id: $('#user_id').val()
+      },
+      complete: function(response){
+        var r = response.responseJSON
+        var home = $('#user_is_home').length ? true : false
+        window.r = r
 
-      // init Timeline
-      DriftMapTimeline.init(r);
+        // init Timeline
+        DriftMapTimeline.init(r);
 
-      // init Leaflet
-      DriftMapLeaflet.init(r);
+        // init Leaflet
+        DriftMapLeaflet.init(r);
 
-      // init Accordian
-      DriftMapAccordian.init(r, home);
-    }
-  });
-  // End initial AJAX call
+        // init Accordian
+        DriftMapAccordian.init(r, home);
+      }
+    });
 
-  // DOM event listeners
+    // End initial AJAX call
 
-  // edit profile info
-  $('.edit-user-anchor').on('click', function(){
-    $('.alter-user-container').slideDown();
-    $('.profile-details-container').addClass('form-open');
-  });
+    // DOM event listeners
 
-  // cancel edit user
-  $('.cancel-edit-user').on('click', function(){
-    $('.alter-user-container').slideUp();
-    $('.profile-details-container').removeClass('form-open');
-  });
-  // End DOM event listeners
-})
+    // edit profile info
+    $('.edit-user-anchor').on('click', function(){
+      $('.alter-user-container').slideDown();
+      $('.profile-details-container').addClass('form-open');
+    });
+
+    // cancel edit user
+    $('.cancel-edit-user').on('click', function(){
+      $('.alter-user-container').slideUp();
+      $('.profile-details-container').removeClass('form-open');
+    });
+    // End DOM event listeners
+  }
+});
--- app/assets/javascripts/waypoints.js
+++ app/assets/javascripts/waypoints.js
@@ -1,52 +1,54 @@
 /* NEW ACTION */
 $(function(){
-  DriftMapLeaflet.defaultInit()
+  if($('#waypoints_new').length){
+    DriftMapLeaflet.defaultInit()
 
-  // init date-picker-calender
-  $('#datetimepicker1').datetimepicker();
+    // init date-picker-calender
+    $('#datetimepicker1').datetimepicker();
 
-  var photo_index = 0;
-  var locationSet = false;
-  
-  // add photos to blip
-  $(document).on('click', '.add-photo', function(){
-    $(this).data('photo-index', ++photo_index)
-    var index = $(this).data('photo-index')
-    var new_img = '<input class="blip-image" name="photo['+ index +']" accept="image/jpeg, image/gif, image/png, image/jpg" type="file" data-image="'+ index +'" style="margin: 6% 6% 5% 3%; width:100%;">';
-    $('.add-photo').last().after(new_img);
-  });
-
-  // set location of blip
-  window.leafletIndexer[0].leaflet.map.on('click', function(e){
-    window.leafletIndexer[0].leaflet.map.off('click');
-    locationSet = true;
-    setLatLng(e.latlng.lat, e.latlng.lng);
-
-    var marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(window.leafletIndexer[0].leaflet.map)
-    marker.dragging.enable();
-    marker.on('dragend', function(e){
-      setLatLng(e.target.getLatLng().lat, e.target.getLatLng().lng)
+    var photo_index = 0;
+    var locationSet = false;
+    
+    // add photos to blip
+    $(document).on('click', '.add-photo', function(){
+      $(this).data('photo-index', ++photo_index)
+      var index = $(this).data('photo-index')
+      var new_img = '<input class="blip-image" name="photo['+ index +']" accept="image/jpeg, image/gif, image/png, image/jpg" type="file" data-image="'+ index +'" style="margin: 6% 6% 5% 3%; width:100%;">';
+      $('.add-photo').last().after(new_img);
     });
-  });
 
-  // submit form
-  $('.commit').on('click', function(){
-    if(locationSet == false) {
-      alert('A location must be set, click anywhere on the map to set your blip.');
-      return false;
-    } else {
-      var date = $('#datepicker-input').val();
-      var mm =   date.slice(0, 2)
-      var dd =   date.slice(3, 5)
-      var yyyy = date.slice(6, 10);
-      $('#waypoint_date').val(yyyy + mm + dd);
-      $('form').submit();
+    // set location of blip
+    window.leafletIndexer[0].leaflet.map.on('click', function(e){
+      window.leafletIndexer[0].leaflet.map.off('click');
+      locationSet = true;
+      setLatLng(e.latlng.lat, e.latlng.lng);
+
+      var marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(window.leafletIndexer[0].leaflet.map)
+      marker.dragging.enable();
+      marker.on('dragend', function(e){
+        setLatLng(e.target.getLatLng().lat, e.target.getLatLng().lng)
+      });
+    });
+
+    // submit form
+    $('.commit').on('click', function(){
+      if(locationSet == false) {
+        alert('A location must be set, click anywhere on the map to set your blip.');
+        return false;
+      } else {
+        var date = $('#datepicker-input').val();
+        var mm =   date.slice(0, 2)
+        var dd =   date.slice(3, 5)
+        var yyyy = date.slice(6, 10);
+        $('#waypoint_date').val(yyyy + mm + dd);
+        $('form').submit();
+      }
+    });
+
+    function setLatLng(lat, lng) {
+      $('#waypoint_x').val(lat);
+      $('#waypoint_y').val(lng);      
     }
-  });
-
-  function setLatLng(lat, lng) {
-    $('#waypoint_x').val(lat);
-    $('#waypoint_y').val(lng);      
   }
 });
 
