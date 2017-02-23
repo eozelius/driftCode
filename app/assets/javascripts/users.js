8e37aaac2f04ab50cf3f9815ae2e40aa2d478a58
ethan ozelius <e.ozelius@gmail.com>
2017-02-23 15:29:24 -0500

    javascripts are loaded on a controller basis

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,66 @@
+/* NEW ACTION */
+$(function(){
+  // Cannot upload images larger than 3M
+  $('.signup').on('click', function(){
+    $('#new_user input[type="text"]').each(function(){
+      $(this).val(xss_trim($(this).val()));
+    });
+
+    $('#new_user').submit();
+  });
+
+  $('#user_profile_pic').on('change', function() {
+    var size_in_megabytes = this.files[0].size/1024/1024;
+    if (size_in_megabytes > 3){
+      alert('Maximum file size is 3MB. Please choose a smaller file.');
+      return false;
+    }
+  });
+})
+
+/* CREATE ACTION */
+
+/* UPDATE ACTION */
+
+
+/* SHOW ACTION */
+$(function(){
+  $.ajax({
+    url: '/api_profile_page',
+    method: 'POST',
+    dataType: 'JSON',
+    data: { 
+      id: $('#user_id').val()
+    },
+    complete: function(response){
+      var r = response.responseJSON
+      var home = $('#user_is_home').length ? true : false
+      window.r = r
+
+      // init Timeline
+      DriftMapTimeline.init(r);
+
+      // init Leaflet
+      DriftMapLeaflet.init(r);
+
+      // init Accordian
+      DriftMapAccordian.init(r, home);
+    }
+  });
+  // End initial AJAX call
+
+  // DOM event listeners
+
+  // edit profile info
+  $('.edit-user-anchor').on('click', function(){
+    $('.alter-user-container').slideDown();
+    $('.profile-details-container').addClass('form-open');
+  });
+
+  // cancel edit user
+  $('.cancel-edit-user').on('click', function(){
+    $('.alter-user-container').slideUp();
+    $('.profile-details-container').removeClass('form-open');
+  });
+  // End DOM event listeners
+})
---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,22 @@
+/* INDEX ACTION */
+$(function(){
+  $.ajax({
+    url: '/api_home',
+    method: 'POST',
+    dataType: 'JSON',
+    complete: function(response) {
+      var r = response.responseJSON;
+      window.r = r
+
+      // init Timeline
+      DriftMapTimeline.init(r);
+
+      // init Leaflet
+      DriftMapLeaflet.init(r);
+
+      // init Accordian
+      DriftMapAccordian.init(r);
+    }
+  });
+});
+
