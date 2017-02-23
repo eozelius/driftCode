6609b88f6811d192bb006b9cd4f4b44772b3e8a8
ethan ozelius <e.ozelius@gmail.com>
2017-02-23 14:37:03 -0500

    unstable

--- app/assets/javascripts/waypoints_new.js
+++ app/assets/javascripts/waypoints_new.js
@@ -1,44 +1,5 @@
 $(function(){
-	/*
-	[{
-		journey: { 
-			id: 51, 
-			description: "My gonzo adventure into Colombia in…", 
-			title: "2015 - Colombia", 
-			driftmap_id: 1, 
-			created_at: "2016-12-03T20:05:51.694Z", 
-			updated_at: "2016-12-03T20:05:51.694Z", 
-			coverphoto: {	url: 'assets/asfd.png' },
-			timeline: < timelineJS3 data >
-		},
-		
-		waypoints: [{ 
-			id: 10, 
-			title: "Sunnyside Queens", 
-			body: "Queens NY.  From Flushing to Astori…", 
-			x: 40.743, 
-			y: -73.922, 
-			driftmap_id: 1, 
-			journey_id: 51, 
-			date:  { year: 2016, month: 11, day: 23 } 
-		}, {}, ...]
-	}, ...]
-	*/
-
-	// Leaflet
-	var data = [{
-		journey: {
-			id: $('#journey_id').val(),
-			description: '',
-			description: '',
-			coverphoto: '',
-		},
-
-		waypoints: [{
-
-		}]
-	}
-
+	DriftMapLeaflet.defaultInit()
 
 	// init date-picker-calender
 	$('#datetimepicker1').datetimepicker();
@@ -55,14 +16,14 @@ $(function(){
 	});
 
 	// set location of blip
-	window.map.on('click', function(e){
-		window.map.off('click');
+	window.leafletIndexer[0].leaflet.map.on('click', function(e){
+		window.leafletIndexer[0].leaflet.map.off('click');
 		locationSet = true;
 		setLatLng(e.latlng.lat, e.latlng.lng);
 
-		var m = L.marker([e.latlng.lat, e.latlng.lng]).addTo(window.map)
-		m.dragging.enable();
-		m.on('dragend', function(e){
+		var marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(window.leafletIndexer[0].leaflet.map)
+		marker.dragging.enable();
+		marker.on('dragend', function(e){
 			setLatLng(e.target.getLatLng().lat, e.target.getLatLng().lng)
 		});
 	});
@@ -73,11 +34,11 @@ $(function(){
 			alert('A location must be set, click anywhere on the map to set your blip.');
 			return false;
 		} else {
-			var date = $('#blip_date').val();
+			var date = $('#datepicker-input').val();
 			var mm = 	 date.slice(0, 2)
 			var dd = 	 date.slice(3, 5)
 			var yyyy = date.slice(6, 10);
-			$('#blip_date').val(yyyy + mm + dd);
+			$('#waypoint_date').val(yyyy + mm + dd);
 			$('form').submit();
 		}
 	});
