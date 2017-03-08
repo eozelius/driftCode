9069ddf05dcdd79299aee7edfc9c3859b3fa46b5
ethan ozelius <e.ozelius@gmail.com>
2017-03-08 09:25:50 -0500

    remove old references to @user.name;  build out friend model

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,2 +1,23 @@
 class Friend < ActiveRecord::Base
+  # ActiveRecord
+  belongs_to :waypoint
+
+  # Attributes
+  attr :first_name
+  attr :last_name
+  attr :photo
+  attr :description
+  attr :x
+  attr :y
+  attr :member
+  attr :visible
+
+  # Validations
+  validates :first_name,  presence: true, length: { minimum: 4, maximum: 120 }
+  validates :description, presence: true, length: { minimum: 4, maximum: 1000 }
+  validates :x, presence: true
+  validates :y, presence: true
+
+  # Methods
+
 end
--- app/models/waypoint.rb
+++ app/models/waypoint.rb
@@ -1,6 +1,7 @@
 class Waypoint < ActiveRecord::Base
   belongs_to :journey
   has_many :waypoint_images, dependent: :destroy
+  has_many :friends, dependent: :destroy
 
   validates :title,   :presence => true, :length => { minimum: 4, maximum: 120 }
   validates :journey_id, :presence => true
