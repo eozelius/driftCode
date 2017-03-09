e34867b1a2c26b87fe8c28f1437ea240e28bfb0f
ethan ozelius <e.ozelius@gmail.com>
2017-03-09 17:11:16 -0500

    Waypoints can now be created from existing journeys

--- app/models/waypoint.rb
+++ app/models/waypoint.rb
@@ -3,10 +3,10 @@ class Waypoint < ActiveRecord::Base
   has_many :waypoint_images, dependent: :destroy
   has_many :friends, dependent: :destroy
 
-  validates :title,   presence: true, length: { minimum: 4, maximum: 120 }
+  validates :title,      presence: true, length: { minimum: 4, maximum: 120 }
   validates :journey_id, presence: true
-  validates :x,		  presence: true
-  validates :y, 	  presence: true
+  validates :x,          presence: true
+  validates :y,          presence: true
   validates :coverphoto, presence: true
 
   mount_uploader :coverphoto, PictureUploader
