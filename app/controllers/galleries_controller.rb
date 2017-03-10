79ceb63e66f641ac8acca578908f2fe1b604655a
ethan ozelius <e.ozelius@gmail.com>
2017-03-10 16:55:10 -0500

    built out scaffolding from gallery model;  all models complete; more or less stable

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -4,7 +4,18 @@ class GalleriesController < ApplicationController
   end
 
   def create
+    @waypoint = Waypoint.find(params[:waypoint_id])
+    if @waypoint.present?
+      @gallery = @waypoint.galleries.create(gallery_params)
 
+      if @gallery.valid?
+        flash[:success] = "gallery successfully created"
+        redirect_to current_user
+      else
+        flash[:danger] = "whoops, something went wrong" 
+        render 'new'
+      end
+    end
   end
 
   def edit
