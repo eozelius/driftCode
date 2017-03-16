8588805694be11f728e22546fab1e6eea27a3b5f
ethan ozelius <e.ozelius@gmail.com>
2017-03-16 18:09:48 -0400

    welcome controller uses all ethan's journeys.  adjust site desc.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
 	def index
 	  	@ethan = User.find(1)
-	  	@journeys = @ethan.driftmap.journeys.last
+	  	@journeys = @ethan.driftmap.journeys
 	end
 end
