88925bdb18416cdf44d8016b69d04571d2dedf11
ethan ozelius <e.ozelius@gmail.com>
2017-07-02 01:02:52 -0400

    no change, just a helpful comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,67 @@
 require 'open-uri'
 
+ethan = User.find_by(email: 'e.ozelius@gmail.com')
+
+colombia = ethan.driftmap.journeys.create!(
+  title: '[POC]2015 - Colombia',
+  description: 'A Digital Nomad trek, laced with Gonzo adventure into the heart and cities of Colombia.',
+  coverphoto: File.open("#{Rails.root}/ethans_photos/2015_colombia/coverphoto.jpg"),
+  x: 4.6077624,
+  y: -74.0745113
+)
+
+bogota = colombia.waypoints.create!(
+  title: 'bogota',
+  body: 'The Capital and largest city in Colombia, Bogota is home to more than 9 million.  The majority of my time I spent in Chapinero, a neighborhood to the north of the old town, tucked in the foot of the massive mountains that overlook the city, and give Bogota it\'s rainy climate.  My days in Bogota were sometimes energetic, sometimes dreary but always informative.  As the first city I would visit, I dove and stumbled headfirst into the language and culture of Colombians.  Bogota was my introduction into the lives, dance, and struggle everyday Colombians; ground zero for my nomadic travels, where I learned the fundamentals of Spanish and the only logical place to start my journey'.html_safe,
+  x: 4.6077624, 
+  y: -74.0745113,
+  date: DateTime.new(2015, 9, 7),
+  coverphoto: File.open("#{Rails.root}/ethans_photos/2015_colombia/bogota_coverphoto.jpg")
+)
+bogota.save
+
+bogota_gallery = bogota.galleries.create!(
+  title: 'bogota gallery',
+  description: 'bogota gallery',
+  coverphoto: File.open("#{Rails.root}/ethans_photos/2015_colombia/bogota_gallery_coverphoto.jpg"),
+  x: 4.6077624, 
+  y: -74.0745113
+)
+
+bogota_gallery.waypoint_images.build(image: File.open("#{Rails.root}/ethans_photos/2015_colombia/gallery_1.jpg"))
+bogota_gallery.waypoint_images.build(image: File.open("#{Rails.root}/ethans_photos/2015_colombia/gallery_2.jpg"))
+bogota_gallery.waypoint_images.build(image: File.open("#{Rails.root}/ethans_photos/2015_colombia/gallery_3.jpg"))
+bogota_gallery.waypoint_images.build(image: File.open("#{Rails.root}/ethans_photos/2015_colombia/gallery_4.jpg"))
+bogota_gallery.save!
+
+philip = bogota.friends.create!(
+  first_name: 'Philip',
+  last_name: '',
+  email: '',
+  photo: open('https://scontent.fbed1-1.fna.fbcdn.net/v/t31.0-8/13235210_1264222140273340_3589384723710979906_o.jpg?oh=4f4904b6f87a7322d410c80cc63ea1cc&oe=59D7E23C'),
+  x: 4.6077624,
+  y: -74.0745113,
+  member: false,
+  visible: true,
+  description: 'Resilience is imperative for any drifting traveler, whether mental physical or humerous the road winds and can up end you at a moments notice.  Towards the end of my virgin expedition outside of the US and separted from my friends, I felt discouraged and found myself brooding as much as sweating poolside at a hostel.  That\'s where I met a amiable German man named Philip, who immediately lifted me back up, mentally and physically.  We wandered around a sticky downtown panama city, shielding our eyes from decadence, and lowered them from poverty.  Eventually we ventured into the Hard Rock Cafe Hotel, one of the tallest building overlooking the city.  We somehow skirted security and found our way to a floor well above our paygrade, and crept up the final 20 flights of stairs to a vacant rooftop restaurant.  We owned the roof and city, indulging in photos and jokes, taking our time to peruse all the views from the top floor.  We never were captured, much less given a questioning eye.  '.html_safe,
+)
+philip.save
+
+couch_surfing = bogota.essays.create!(
+  title: 'A Couch Surfing life',
+  body: 'Couch Surfing and the people I have met via surfing or hosting, will always have a special place in my heart.  Couchsurfing.com embodies the idea of realworld benefits only possible via an online community, on a greater scale than (in my opinion) any other website.  The community is at the same time so vast and far-flung, small and tight-knit, with diversity and common ground.  Some of my greatest friends, and most of my greatest experiences, I owe to couchsurfing.com.  My first experience surfing, came at the home of a Bogotano friend that I had hosted in my own home only a couple months before.  When I arrived at his home, where he lived with his parents, cat and dog, I was greeted (in solely Spanish of course) warmly and given all the amenitites a humble traveler could ask for.  I also arrived with less than 50 spanish words in my vocabulary, and no idea what I was supposed to do next.  Thankfully, my hosts aided me in spanish lessons, advice on the best that Colombia has to offer, and several portions the infamous Aguardiente.  In the mornings, I would have breakfast with the family, where the my friend Harold\'s mother would explain grammatical oddities of spanish, in spanish.  It was this immersion that quashed any final fears of speaking spanish, if I wanted breakfast, I asked for desayuno, when I needed to go to the embassy, I told informed family \"Tengo que ir a la embajada\!"  I am extremely thankful to my Colombian family who guided me in the first few days of my Gonzo adventure, and all the friends I have made from couchsurfing.com will always have a place to crash in my home.'.html_safe,
+  x:  4.6077624,
+  y: -74.0745113,
+  coverphoto: open('https://www.worldwideinsure.com/travel-blog/wp-content/uploads/2013/05/couchsurfing_logo.png'),
+)
+
+couch_surfing.save
+colombia.save
+
+
+=begin
+require 'open-uri'
+
 e = User.find_by(email: 'e.ozelius@gmail.com')
 
 e.destroy unless e.nil?
@@ -86,9 +148,7 @@ couch_surfing = bogota.essays.create!(
 
 couch_surfing.save
 colombia.save
-
-
-
+=end
 
 
 
