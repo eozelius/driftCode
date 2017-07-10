450e5ce9d5ea03c52e43ad42e0c15e6e605bd937
ethan ozelius <e.ozelius@gmail.com>
2017-07-10 15:03:11 -0400

    feed is toggled off by default;

--- app/assets/javascripts/driftmaps.js
+++ app/assets/javascripts/driftmaps.js
@@ -1,9 +1,4 @@
 $(function(){
-  // NEW ////
-  /*if($('#driftmap_new').length){
-
-  }*/
-  
   // EDIT ////
   if($('#driftmap_edit').length){
     DriftMapLeaflet.defaultInit();
@@ -13,10 +8,6 @@ $(function(){
       var lat = window.m.getCenter().lat;
       var lng = window.m.getCenter().lng;
       var zoom = window.m.getZoom();
-
-      console.log('lat: ' + lat)
-      console.log('lng: ' + lng)
-      console.log('zooom: ' + zoom)
       $('#driftmap_init_x').val(lat);
       $('#driftmap_init_y').val(lng);
       $('#driftmap_init_zoom').val(zoom);
